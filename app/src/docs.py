907187516742f3068d1cfa7fb07ce3ce005e43a1
César P <cesarlpb@gmail.com>
2022-11-22 14:07:46 +0000

    Creamos documentación

---
+++ app/src/docs.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# 127.0.0.1:5000
+# 80.240.127.173:5000
+@app.route("/")
+@app.route("/docs")
+def hello():
+    return render_template("docs.html")
+
+# 5000
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=5010, debug = True)
