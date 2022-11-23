1f878ae9038c3975fb1e1a74ad5093ea59197711
César <cesarlpb@gmail.com>
2022-11-23 09:27:18 +0100

    Sintaxis en Python

--- app/learn-python/1.syntax.py
+++ app/learn-python/1.syntax.py
@@ -1,3 +1,32 @@
-#%%
+#%% Hola, mundo
 print("Hola mundo")
-#%% 
+#%% Bucle if
+if 5 > 2:
+    print("5 es mayor que 2")
+    print("Otro print dentro del if")
+print("Fuera del if")
+
+#%% Variables
+x = 1
+str = "string"
+myDict = {"key": "value"}
+arr = [1, 2, 3]
+
+# %%
+# Comentario
+print("algo") # comentario
+""" 
+    Comentario 
+
+    en varias líneas
+"""
+''' 
+Esto también es 
+
+de varias líneas 
+'''
+'También cuenta como comentario'
+# Línea 1
+# Línea 2
+# Línea 3
+# %%
