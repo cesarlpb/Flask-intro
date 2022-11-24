26c98ce9486a95835cf15d86629033f86f1253d2
César P <cesarlpb@gmail.com>
2022-11-24 15:14:21 +0000

    Rutas dinámicas con tipado

--- app/learn-flask/1.routes.py
+++ app/learn-flask/1.routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, render_template, request
  
 app = Flask(__name__)
 
@@ -13,6 +13,12 @@ def hello(name = ""):
 @app.route("/user/<string:user>")
 def user(user = ""):
     return f"Hola usuario {user}!"
+
+@app.route("/products")
+@app.route("/products/<string:_category>")
+@app.route("/products/<string:_category>/<int:_id>")
+def products(_category = "", _id = 1):
+    return render_template("routes.html", category=_category, id=_id)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5011, debug = True)
